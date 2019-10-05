49d7f6c417560b1fb7bedebc0e2818a417b5d165
shijominagashiroq@gmail.com <shijominagashiroq@gmail.com>
2019-10-05 10:18:06 -0400

    Xamarin

---
+++ mvcAddress/appAddress/appAddress/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿
+namespace appAddress.Models
+{
+    
+
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+        twitter,
+        instagram,
+        dblink
+    }
+
+    public class Book
+    {
+        
+        public int bookID { get; set; }
+        public string name { get; set; }
+        public TypeContact type { get; set; }
+        public string contact { get; set; }
+    }
+}
---
+++ mvcAddress/appAddress/appAddress/Models/Response.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace appAddress.Models
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
